481d1aa1220ed4b452b69716506924cbce84c5aa
RACHIT RAWAT <95635515+Lucky-rawat@users.noreply.github.com>
2022-07-07 23:02:46 +0530

    Create max-heap.js

---
+++ max-heap.js
@@ -0,0 +1,79 @@
+  class MaxHeap {
+    constructor(){
+        this.values = [];
+    }
+parent(index) {
+    return Math.floor((index - 1) / 2);
+}
+leftChild(index) {
+    return (index * 2) + 1;
+}
+rightChild(index) {
+    return (index * 2) + 2;
+}
+isLeaf(index) {
+    return (
+        index >= Math.floor(this.values.length / 2) && index <= this.values.length - 1
+    )
+}
+swap(index1, index2) {
+    [this.values[index1], this.values[index2]] = [this.values[index2], this.values[index1]];
+}
+add(element) {
+    this.values.push(element);
+    this.heapifyUp(this.values.length - 1);
+}
+heapifyUp(index) {
+    let currentIndex = index,
+        parentIndex = this.parent(currentIndex);
+    while (currentIndex > 0 && this.values[currentIndex] > this.values[parentIndex]) {
+        this.swap(currentIndex, parentIndex);
+        currentIndex = parentIndex;
+        parentIndex = this.parent(parentIndex);
+    }
+}
+extractMax() {
+    if (this.values.length < 1) return 'heap is empty';
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+    this.heapifyDown(0);
+    return max;
+}
+
+heapifyDown(index) {
+    if (!this.isLeaf(index)) {
+        let leftChildIndex = this.leftChild(index),
+            rightChildIndex = this.rightChild(index),
+            largestIndex = index;
+        if (this.values[leftChildIndex] > this.values[largestIndex]) {
+            largestIndex = leftChildIndex;
+        }
+        if (this.values[rightChildIndex] >= this.values[largestIndex]) {
+            largestIndex = rightChildIndex;
+        }
+        if (largestIndex !== index) {
+            this.swap(index, largestIndex);
+            this.heapifyDown(largestIndex);
+        }
+    }
+}
+buildHeap(array) {
+    this.values = array;
+
+    for(let i = Math.floor(this.values.length / 2); i >= 0; i--){
+        this.heapifyDown(i);
+    }
+}
+peek() {
+	return this.values[0];
+}
+}
+
+    let i = 0;
+    while (!this.isLeaf(i)) {
+        console.log("PARENT:", this.values[i]);
+        console.log("LEFT CHILD:", this.values[this.leftChild(i)]);
+        console.log("RIGHT CHILD:", this.values[this.rightChild(i)]);
+        i++;
+    }      
